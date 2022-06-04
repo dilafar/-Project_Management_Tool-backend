f9ce16a87466ad7837c9e69ba803cf6838c1bb3a
dilafar <it20784720@my.sliit.lk>
2022-06-04 15:58:49 +0530

    admin dashboard and staff profile view added

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const bodyparser = require("body-parser");
 require("dotenv").config();
+const socketio = require('socket.io');
+const http = require('http');
+const {addUserchat , removeUserchat , getUserchat ,getUsersInRoomchat} = require('./controller/chat');
 
 const app = express();
 
@@ -20,12 +23,15 @@ const panelDataroutes = require("./routes/panelData");
 const requestroutes = require("./routes/request");
 const responseroutes = require("./routes/response");
 const grouprouter = require("./routes/studentGroup");
+const chatroute = require('./routes/chat');
 
 app.use("/user", userroutes);
 app.use("/panel", panelDataroutes);
 app.use("/request", requestroutes);
 app.use("/response", responseroutes);
 app.use("/group", grouprouter);
+
+
 
 mongoose.connect(url ,  {
     useNewUrlParser: true,
@@ -34,8 +40,50 @@ mongoose.connect(url ,  {
 }
 );
 
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on('connection', (socket)=>{
+    
+
+    socket.on('join', ({name , room}, callback)=>{
+        const {error , user} = addUserchat({id: socket.id , name , room});
+
+        if(error) return callback(error); 
 
 
-app.listen(PORT , ()=>{
+        socket.join(user.room);
+         socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.`});
+         socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
+       
+        /*const error = true;
+        if(error){
+            callback({error: 'error'});
+        }*/
+        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+       callback();
+    });
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUserchat(socket.id);
+    
+        io.to(user.room).emit('message', { user: user.name, text: message });
+    
+        callback();
+      });
+
+    socket.on('disconnect',()=>{
+        const user = removeUser(socket.id);
+
+        if(user) {
+          io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
+          io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+        }
+    })
+});
+
+app.use(chatroute);
+
+server.listen(PORT , ()=>{
             console.log(`Server connected to : ${PORT}`);
 });
